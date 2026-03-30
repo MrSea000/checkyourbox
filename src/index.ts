0137b2fd551fbf3847136ee2ac6f667dc1ca3a3c
MrSea000 <mrsea0@outlook.com>
2026-03-30 21:46:51 +0800

    111

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,9 @@ export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context, config: Config) {
   // write your plugin here
+  ctx.on('message', (session) => {
+    if (session.content === '天王盖地虎') {
+      session.send('宝塔镇河妖')
+    }
+  })
 }
